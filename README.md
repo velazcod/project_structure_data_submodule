3c79683414ec5aefab388329bbb88eb1aca19341
Daniel Velazco <velazcod@gmail.com>
2015-06-04 15:18:12 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# project_structure_data_submodule
---
+++ src/main/java/com/danvelazco/projecttesthelloworld/ModuleDataObject.java
@@ -0,0 +1,18 @@
+package com.danvelazco.projecttesthelloworld;
+
+/**
+ * @since 6/4/15
+ */
+public class ModuleDataObject {
+
+    private String mName;
+
+    public ModuleDataObject(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+}
